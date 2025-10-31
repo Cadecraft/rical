f658941a9b32f74c1f38e62284886eb7ffbfa3ba
Cadecraft <cadendotlee@gmail.com>
2025-10-31 10:32:23 -0700

    feat: basic rical web file structure

---
+++ rical_web/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Rical Web</h1>
+      <div className="card">
+        Coming soon!
+        <br />
+        <a href="https://github.com/Cadecraft/rical">GitHub</a>
+      </div>
+    </>
+  )
+}
+
+export default App
