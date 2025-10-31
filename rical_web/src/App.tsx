0b050117b9a539558d32297257ce0cde678d6b52
Cadecraft <cadendotlee@gmail.com>
2025-10-31 10:51:56 -0700

    feat: landing page

--- rical_web/src/App.tsx
+++ rical_web/src/App.tsx
@@ -2,14 +2,25 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Rical Web</h1>
-      <div className="card">
-        Coming soon!
-        <br />
-        <a href="https://github.com/Cadecraft/rical">GitHub</a>
+    <div className="outer">
+      <div className="inner">
+        <div className="banner">
+          <h1>Rical</h1>
+          <span>The latest calendar app for minimalists</span>
+        </div>
+        <div className="section">
+          <h2>Get started</h2>
+          <span className="secondary">
+            Rical Web is coming soon! For now, you'll have to use the TUI
+          </span>
+        </div>
+        <div className="section footer">
+          © 2025 rical contributors
+          <br />
+          <a href="https://github.com/Cadecraft/rical">GitHub</a>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
